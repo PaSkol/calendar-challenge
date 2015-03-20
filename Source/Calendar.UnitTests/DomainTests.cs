558a76671adc9ca38eda19c4963ab6b1e9ea585f
PaSkol <paskol@op.pl>
2015-03-20 20:28:23 +0100

    Created solution
    Added project for domain and project for tests

---
+++ Source/Calendar.UnitTests/DomainTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calendar.UnitTests
+{
+	[TestClass]
+	public class DomainTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
