b68746ce912f64bb2dcefa19ee0850cd292d313c
PaSkol <paskol@op.pl>
2015-03-22 21:23:44 +0100

    Rename property of Event class

--- Source/Calendar.Domain/Event.cs
+++ Source/Calendar.Domain/Event.cs
@@ -13,7 +13,10 @@ namespace Calendar.Domain
 		public string Title { get; set; }
 		public string Description { get; set; }
 		public string Location { get; set; }
-		public Interval Interval { get; set; }
+		/// <summary>
+		/// Interval of time when event occurs
+		/// </summary>
+		public Interval Occurs { get; set; }
 		public bool UserIsBusy { get; set; }
 		public Visibility Visibility { get; set; }
 		public TimeZone TimeZone { get; set; }
